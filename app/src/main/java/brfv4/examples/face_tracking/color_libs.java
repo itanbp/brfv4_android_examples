01482b6cebbb90ccdc77616251e6ce5dcb0b1b22
itan <itanbp@gmail.com>
2018-05-28 17:44:20 +0300

    code indentation

--- app/src/main/java/brfv4/examples/face_tracking/color_libs.java
+++ app/src/main/java/brfv4/examples/face_tracking/color_libs.java
@@ -15,73 +15,73 @@ import brfv4.geom.Rectangle;
 public class color_libs extends BRFBasicJavaExample {
 
 
-	public color_libs(Context context) {
-		super(context);
-	}
+    public color_libs(Context context) {
+        super(context);
+    }
 
-	@Override
-	public void initCurrentExample(BRFManager brfManager, Rectangle resolution) {
+    @Override
+    public void initCurrentExample(BRFManager brfManager, Rectangle resolution) {
 
-		Log.d("BRFv4", "BRFv4 - intermediate - face tracking - color libs.\n" +
-				"Draws triangles with a certain fill color.");
+        Log.d("BRFv4", "BRFv4 - intermediate - face tracking - color libs.\n" +
+                "Draws triangles with a certain fill color.");
 
-		brfManager.init(resolution, resolution, _appId);
-	}
+        brfManager.init(resolution, resolution, _appId);
+    }
 
-	@Override
-	public void updateCurrentExample(BRFManager brfManager, Bitmap imageData, DrawingUtils draw) {
+    @Override
+    public void updateCurrentExample(BRFManager brfManager, Bitmap imageData, DrawingUtils draw) {
 
-		brfManager.update(imageData);
+        brfManager.update(imageData);
 
-		draw.clear();
+        draw.clear();
 
-		// Face detection results: a rough rectangle used to start the face tracking.
+        // Face detection results: a rough rectangle used to start the face tracking.
 
-		draw.drawRects(brfManager.getAllDetectedFaces(),	false, 1.0, 0x00a1ff, 0.5);
-		draw.drawRects(brfManager.getMergedDetectedFaces(),	false, 2.0, 0xffd200, 1.0);
+        draw.drawRects(brfManager.getAllDetectedFaces(), false, 1.0, 0x00a1ff, 0.5);
+        draw.drawRects(brfManager.getMergedDetectedFaces(), false, 2.0, 0xffd200, 1.0);
 
-		Vector<BRFFace> faces = brfManager.getFaces();
+        Vector<BRFFace> faces = brfManager.getFaces();
 
-		for(int i = 0; i < faces.size(); i++) {
+        for (int i = 0; i < faces.size(); i++) {
 
-			BRFFace face = faces.get(i);
+            BRFFace face = faces.get(i);
 
-			if(		face.state.equals(brfv4.BRFState.FACE_TRACKING_START) ||
-					face.state.equals(brfv4.BRFState.FACE_TRACKING)) {
+            if (face.state.equals(brfv4.BRFState.FACE_TRACKING_START) ||
+                    face.state.equals(brfv4.BRFState.FACE_TRACKING)) {
 
-				// Face tracking results: 68 facial feature points.
+                // Face tracking results: 68 facial feature points.
 
-				draw.drawTriangles(	face.vertices, face.triangles, false, 1.0, 0x00a0ff, 0.4);
-				draw.drawVertices(	face.vertices, 2.0, false, 0x00a0ff, 0.4);
+                draw.drawTriangles(face.vertices, face.triangles, false, 1.0, 0x00a0ff, 0.4);
+                draw.drawVertices(face.vertices, 2.0, false, 0x00ff79, 1);
 
-				// Now just draw all the triangles of the mouth in a certain color.
+                // Now just draw all the triangles of the mouth in a certain color.
 
-				draw.fillTriangles(	face.vertices, libTriangles, false, 0x00ff79, 0.8);
-			}
-		}
-	}
+                draw.fillTriangles(face.vertices, libTriangles, false, 0x00ff79, 0.8);
+            }
+        }
+    }
 
-	private int[] libTriangles	= {
-			48, 49, 60,
-			48, 59, 60,
-			49, 50, 61,
-			49, 60, 61,
-			50, 51, 62,
-			50, 61, 62,
-			51, 52, 62,
-			52, 53, 63,
-			52, 62, 63,
-			53, 54, 64,
-			53, 63, 64,
-			54, 55, 64,
-			55, 56, 65,
-			55, 64, 65,
-			56, 57, 66,
-			56, 65, 66,
-			57, 58, 66,
-			58, 59, 67,
-			58, 66, 67,
-			59, 60, 67
+    private int[] libTriangles = {
+            48, 49, 60,
+            48, 59, 60,
+            49, 50, 61,
+            49, 60, 61,
+            50, 51, 62,
+            50, 61, 62,
+            51, 52, 62,
+            52, 53, 63,
+            52, 62, 63,
+            53, 54, 64,
+            53, 63, 64,
+            54, 55, 64,
+            55, 56, 65,
+            55, 64, 65,
+            56, 57, 66,
+            56, 65, 66,
+            57, 58, 66,
+            58, 59, 67,
+            58, 66, 67,
+            59, 60, 67
 //			,					// mouth whole
 //			 60, 61, 67,
 //			 61, 62, 66,
@@ -89,5 +89,5 @@ public class color_libs extends BRFBasicJavaExample {
 //			 62, 63, 66,
 //			 63, 64, 65,
 //			 63, 65, 66
-	};
+    };
 }
